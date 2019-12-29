a9cbc491f1c32320cc33a7367ffe4f380c26d421
unknown <otodrahonovsky@yahoo.com>
2019-12-29 11:35:52 +0800

    Authentication and Authorization

---
+++ Paycompute.Persistence/DataSeedingInitializer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Paycompute.Persistence
+{
+    public static class DataSeedingInitializer
+    {
+        public static async Task UserAndRoleSeedAsync(UserManager<IdentityUser> userManager,
+                                                RoleManager<IdentityRole> roleManager)
+        {
+            string[] roles = { "Admin", "Manager", "Staff" };
+            foreach(var role in roles)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(role);
+                if (!roleExist)
+                { 
+                    IdentityResult result = await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            if (userManager.FindByEmailAsync("everest@codewitheverest.com").Result == null)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = "everest@codewitheverest.com",
+                    Email = "everest@codewitheverest.com"
+                };
+                IdentityResult identityResult = userManager.CreateAsync(user, "Password1").Result;
+                if (identityResult.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+
+            if (userManager.FindByEmailAsync("manager@gmail.com").Result == null)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = "manager@gmail.com",
+                    Email = "manager@gmail.com"
+                };
+                IdentityResult identityResult = userManager.CreateAsync(user, "Password1").Result;
+                if (identityResult.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Manager").Wait();
+                }
+            }
+
+            if (userManager.FindByEmailAsync("jane.doe@gmail.com").Result == null)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = "jane.doe@gmail.com",
+                    Email = "jane.doe@gmail.com"
+                };
+                IdentityResult identityResult = userManager.CreateAsync(user, "Password1").Result;
+                if (identityResult.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Staff").Wait();
+                }
+            }
+
+            if (userManager.FindByEmailAsync("john.doe@gmail.com").Result == null)
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = "john.doe@gmail.com",
+                    Email = "john.doe@gmail.com"
+                };
+                IdentityResult identityResult = userManager.CreateAsync(user, "Password1").Result;
+                //No role assigned to John Doe
+            }
+        }
+    }
+}
